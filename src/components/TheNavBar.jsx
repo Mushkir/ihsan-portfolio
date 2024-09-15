e00c71cad7351794c112beb5635105125a179af7
Mushkir <mushkirmohamed@gmail.com>
2024-09-16 02:09:14 +0530

    style: designed homepage

--- src/components/TheNavBar.jsx
+++ src/components/TheNavBar.jsx
@@ -1,36 +1,106 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const TheNavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="bg-primary-black flex justify-between items-center px-14 py-5">
-      <h3 className="text-3xl font-bold text-yellow-400">Subair Ihsan</h3>
+    <nav className="relative z-10">
+      <div className="bg-primary-black flex justify-between items-center px-14 py-5">
+        <h3 className="text-3xl font-bold text-yellow-400">Subair Ihsan</h3>
 
-      <ul className="text-white flex space-x-8" id="activeMenu">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
+        <ul
+          className={`text-white space-x-8  hidden xl:flex items-center text-xl ${
+            isOpen ? "block" : "hidden"
+          }`}
+          id="activeMenu"
+        >
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
 
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
 
-        <li>
-          <NavLink to="/experience">Experience</NavLink>
-        </li>
+          <li>
+            <NavLink to="/experience">Experience</NavLink>
+          </li>
 
-        <li>
-          <NavLink to="/education">Edutcation</NavLink>
-        </li>
+          <li>
+            <NavLink to="/education">Education</NavLink>
+          </li>
 
-        <li>
-          <NavLink to="/skills">Skills</NavLink>
-        </li>
+          <li>
+            <NavLink to="/skills">Skills</NavLink>
+          </li>
 
-        <li>
-          <NavLink to="/resume">Resume</NavLink>
-        </li>
-      </ul>
-    </div>
+          <li>
+            <NavLink to="/resume">Resume</NavLink>
+          </li>
+        </ul>
+
+        <div className="xl:hidden">
+          <button
+            className="focus:outline-none text-yellow-400"
+            onClick={toggleMenu}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={` text-main-heading-color xl:hidden mt-4 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <ul
+          className="flex flex-col items-end pr-14 bg-primary-black space-y-5 text-white"
+          id="activeMenu"
+        >
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/experience">Experience</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/education">Edutcation</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/skills">Skills</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/resume">Resume</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- src/pages/TheHomePage.jsx
+++ src/pages/TheHomePage.jsx
@@ -1,7 +1,45 @@
+import { Link } from "react-router-dom";
+
 const TheHomePage = () => {
   return (
-    <div className="font-nunito">
-      <h1>Welcome to the home page</h1>
+    <div className="font-nunito bg-primary-black px-14 py-8 flex justify-between pr-[10rem] h-screen pt-[150px]">
+      {/* Hero name info */}
+      <div>
+        <small className="text-yellow-400 mb-4 block text-lg">Hello!</small>
+        <h2 className="text-white text-7xl font-semibold">
+          I&apos;m <span className="text-yellow-400">Subair Ihsan</span>
+        </h2>
+        <span className="text-white text-lg block my-4 font-semibold">
+          Quantity Surveyor
+        </span>
+
+        <div className="flex items-center gap-x-5">
+          {/* Contact me button */}
+          <button
+            type="button"
+            className="bg-yellow-400 px-5 py-2 rounded-lg hover:opacity-95 hover:font-semibold hover:transition 500"
+          >
+            Contact me
+          </button>
+
+          {/* Resume button */}
+          <Link
+            className="text-white border border-yellow-400 px-5 py-2 rounded-lg hover:font-semibold hover:bg-yellow-400 hover:text-black hover:transition 500"
+            to={"#"}
+          >
+            Resume
+          </Link>
+        </div>
+      </div>
+
+      {/* Hero img */}
+      <div>
+        <img
+          className="w-[400px] h-[400px] rounded-full border-4 border-yellow-400 object-cover object-center"
+          src="/ihsan_profile.JPEG"
+          alt="Ihsan Momz image"
+        />
+      </div>
     </div>
   );
 };
