d30e730630103d1c0b5a9e4db1574200f0c4ac49
Mushkir <mushkirmohamed@gmail.com>
2024-09-16 01:20:20 +0530

    feat: implemented active menu indication feature

--- src/components/TheNavBar.jsx
+++ src/components/TheNavBar.jsx
@@ -1,33 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const TheNavBar = () => {
   return (
     <div className="bg-primary-black flex justify-between items-center px-14 py-5">
       <h3 className="text-3xl font-bold text-yellow-400">Subair Ihsan</h3>
 
-      <ul className="text-white flex space-x-8">
+      <ul className="text-white flex space-x-8" id="activeMenu">
         <li>
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <NavLink to="/about">About</NavLink>
         </li>
 
         <li>
-          <Link to="/experience">Experience</Link>
+          <NavLink to="/experience">Experience</NavLink>
         </li>
 
         <li>
-          <Link to="/education">Edutcation</Link>
+          <NavLink to="/education">Edutcation</NavLink>
         </li>
 
         <li>
-          <Link to="/skills">Skills</Link>
+          <NavLink to="/skills">Skills</NavLink>
         </li>
 
         <li>
-          <Link to="/resume">Resume</Link>
+          <NavLink to="/resume">Resume</NavLink>
         </li>
       </ul>
     </div>
