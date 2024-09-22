2f0722626c8d0826df2434faf3624c6e4a1572ea
Mushkir <mushkirmohamed@gmail.com>
2024-09-23 03:56:55 +0530

    fix: fixed bug in footer section

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -18,7 +18,7 @@ const TheFooter = () => {
         </Link>
       </p>
 
-      <div className="flex justify-center items-center gap-x-2 sm:gap-x-4 mt-3">
+      <div className="flex justify-center items-center gap-x-2 sm:gap-x-4 mt-3 mb-5">
         {/* LinkedIn */}
         <Link
           to={"https://www.linkedin.com/in/mohamed-mushkir/"}
