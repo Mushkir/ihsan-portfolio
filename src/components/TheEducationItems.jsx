f320544fd4fcdecc61ae9ce0c57af3d5c9f2c941
Mushkir <mushkirmohamed@gmail.com>
2024-10-07 18:39:19 +0530

    style: designed education page

---
+++ src/components/TheEducationItems.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TheEducationItems = ({ name, orgName, country }) => {
+  return (
+    <div className="border rounded-md p-2 sm:p-5 border-yellow-400 mb-5">
+      <h3 className="text-white text-lg flex sm:items-center gap-x-2">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 48 48"
+          >
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeLinejoin="round"
+              strokeWidth="4"
+            >
+              <path
+                fill="currentColor"
+                d="M2 17.4L23.022 9l21.022 8.4l-21.022 8.4z"
+              />
+              <path
+                strokeLinecap="round"
+                d="M44.044 17.51v9.223m-32.488-4.908v12.442S16.366 39 23.022 39c6.657 0 11.467-4.733 11.467-4.733V21.825"
+              />
+            </g>
+          </svg>
+        </div>
+        <strong>{name}</strong>
+      </h3>
+
+      <div>
+        {/* Company Name div */}
+        <div className="flex sm:items-center gap-2 px-1 sm:px-7 mt-1 italic text-gray-300">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M21 10a1 1 0 0 0 1-1V6a1 1 0 0 0-.684-.948l-9-3a1 1 0 0 0-.632 0l-9 3A1 1 0 0 0 2 6v3a1 1 0 0 0 1 1h1v7.184A2.995 2.995 0 0 0 2 20v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1v-2a2.995 2.995 0 0 0-2-2.816V10Zm-1 11H4v-1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1ZM6 17v-7h2v7Zm4 0v-7h4v7Zm6 0v-7h2v7ZM4 8V6.72l8-2.666l8 2.667V8Z"
+              />
+            </svg>
+          </div>
+          <div className="flex flex-wrap items-center">
+            {country ? (
+              <div>
+                <small>{orgName}</small>
+                &nbsp;{" - "}&nbsp;
+                <small>
+                  {" "}
+                  <span className="not-italic">📍{country}</span>
+                </small>
+              </div>
+            ) : (
+              <div>
+                <small>{orgName}</small>
+                {/* &nbsp;{" - "}&nbsp;
+                <small>
+                  {" "}
+                  <span className="not-italic">📍</span>
+                </small> */}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+TheEducationItems.propTypes = {
+  name: PropTypes.string,
+  orgName: PropTypes.string,
+  country: PropTypes.string,
+};
+
+export default TheEducationItems;
--- src/pages/TheEducationPage.jsx
+++ src/pages/TheEducationPage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import TheEducationItems from "../components/TheEducationItems";
+// import { PiBuildingsFill } from "react-icons/pi";
 
 const TheEducationPage = () => {
-  return <div className="text-yellow-400">TheEducationPage</div>;
+  return (
+    <div className="font-nunito bg-primary-black max-w-[1500px] px-2 sm:px-14 py-8 flex sm:pt-[80px] lg:pt-[150px] xl:pt-[190px]">
+      {/* Hero img */}
+      <div className="md:flex mx-auto sm:mx-0 justify-center gap-x-10 items-start mb-10 md:mb-0 lg:mx-20">
+        <img
+          className="w-[200px] h-[200px] mx-auto md:w-[300px] md:h-[300px] rounded-full border-4 border-yellow-400 object-cover object-center mb-5 lg:mb-0"
+          src="/ihsan_profile_1.JPG"
+          alt="Ihsan Momz image"
+        />
+
+        <div>
+          {/* Degree */}
+          <TheEducationItems
+            name={"BSc (Hons) In Quantity Surveying (Following)"}
+            orgName={"De Montfort university (DMU) Leicester (UK)"}
+          />
+
+          {/* HND */}
+          <TheEducationItems
+            name={"Higher National Diploma in Quantity Surveying (Level 5)"}
+            orgName={"IPHS Campus"}
+            country={"Sri Lanka."}
+          />
+
+          {/* Diploma */}
+          <TheEducationItems
+            name={"Diploma in Quantity Surveying"}
+            country={"Sri Lanka. "}
+            orgName={"London Business Academy"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TheEducationPage;
